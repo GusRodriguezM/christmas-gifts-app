6728bda1accd3018b1919a5be7bbcba013ff1c42
Gustavo RM <gustavo.rm.isc@gmail.com>
2022-03-02 21:33:49 -0600

    Step 3: Adding a form, using a custom hook to store the new gift in a state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
+import { useState } from 'react';
+
+import { useForm } from './hooks/useForm';
+
 import './App.css';
 
 function App() {
 
-  const gifts = [
-    {id: 1, name: 'socks'},
-    {id: 2, name: 'ugly sweater'},
-    {id: 3, name: 'Santa\'s hat'},
-    {id: 4, name: 'snow sled'},
-    {id: 5, name: 'snowball gun'},
-  ];
+  const [gifts, setGifts] = useState([
+    {name: 'socks'},
+    {name: 'ugly sweater'},
+    {name: 'Santa\'s hat'},
+    {name: 'snow sled'},
+    {name: 'snowball gun'},
+  ]);
+
+  const [formValues, handleInputChange, reset] = useForm({
+    name: ''
+  });
+
+  const { name } = formValues;
+
+  const handleAddGift = (e) => {
+    e.preventDefault(); 
+    setGifts([...gifts, formValues]);
+    reset();
+  }
 
   return (
     <div className="App">
       <div className='list'>
         <h1>Christmas gifts</h1>
+
+        <form onSubmit={handleAddGift}>
+          <input 
+            type='text'
+            placeholder='Your gift'
+            name='name'
+            value={name}
+            autoComplete='off'
+            onChange={handleInputChange}
+          />
+
+          <button
+            type='submit'
+          >
+            Add a gift
+          </button>
+        </form>
         <ul>
           {
-            gifts.map((gift) => (
-              <li>{gift.name}</li>
+            gifts.map((gift, i) => (
+              <li key={i}>{gift.name}</li>
             ))
           }
         </ul>
