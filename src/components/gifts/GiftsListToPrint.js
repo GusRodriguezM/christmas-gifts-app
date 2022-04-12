b808ab4e80b99449fcce150c0e5a7e1fa674f6b3
Gustavo RM <gustavo.rm.isc@gmail.com>
2022-04-11 22:08:47 -0500

    Extra: Adding a new stylesheet to the GiftsListToPrint

--- src/components/gifts/GiftsListToPrint.js
+++ src/components/gifts/GiftsListToPrint.js
@@ -1,27 +1,29 @@
 import React from 'react';
 
-import '../../styles.css';
+import './GiftsListToPrint.css';
 
 export const GiftsListToPrint = React.forwardRef(({gifts}, ref) => {
     return (
-        // Usingf the ref to print all the content inside the div
-        <div ref={ref}>
 
-            <h1 className='title'>Your list of gifts</h1>
+        <>
+            <h1 className='title'>Your gifts list</h1>
 
-            {
-                gifts.map((gift, i) => (
-                    <div className='main' key={i}>
-                        <img className='gift-image' alt={gift.name} src={gift.image} />
+            {/* Using the ref to print all the content inside the div */}
+            <div ref={ref} className='container'>
+                {
+                    gifts.map((gift, i) => (
+                        <div className='main' key={i}>
+                            <img className='gift-image' alt={gift.name} src={gift.image} />
 
-                        <div className='info'>
-                            <span>{gift.name} ({gift.quantity})</span>
-                            <span>{gift.person}</span>
+                            <div className='info'>
+                                <span>{gift.name} ({gift.quantity})</span>
+                                <span>{gift.person}</span>
+                            </div>
                         </div>
-                    </div>
-                ))
-            }
+                    ))
+                }
+            </div>
+        </>
 
-        </div>
     )
 });
